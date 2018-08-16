13a17f54647a17802251d5a43da4f8f2be185cc5
mkj4489 <masakijo@gmail.com>
2018-08-17 02:25:30 +0900

    Hello organized

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+        System.out.println("Hello World One"); // This is comment
+        System.out.println("Hello World Two");
+        System.out.println("Hello World Three");
     }
 }
